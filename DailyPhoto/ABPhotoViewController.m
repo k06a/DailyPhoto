429478b821a1036a4be9d0791526c9ecb0c3a5e9
Anton Bukov <k06aaa@gmail.com>
2013-12-03 01:14:19 +0400

    A little bit fixed

--- DailyPhoto/ABPhotoViewController.m
+++ DailyPhoto/ABPhotoViewController.m
@@ -104,7 +104,7 @@
     */
     
     [UIView animateWithDuration:0.4
-                          delay:0.1
+                          delay:0.0
                         options:(UIViewAnimationOptionCurveEaseInOut)
                      animations:^{
                          self.view.backgroundColor = [UIColor blackColor];
