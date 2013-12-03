974fc9f33a8569e110946cabac66c0e3434574de
Anton Bukov <k06aaa@gmail.com>
2013-12-03 12:10:22 +0400

    Flip animation (with some problems)

--- DailyPhoto/ABPhotoViewController.m
+++ DailyPhoto/ABPhotoViewController.m
@@ -88,9 +88,132 @@
     }];
 }
 */
+- (CAKeyframeAnimation *)firstHalfFlipAnimation:(CGRect)transAndScaleRect
+{
+    CATransform3D an0 = CATransform3DIdentity;
+    CATransform3D an1 = [self endOfFirstHalfTransform];
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.values = @[[NSValue valueWithCATransform3D:an0],
+                         [NSValue valueWithCATransform3D:an1]];
+    animation.duration = 0.3;
+    animation.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]];
+    
+    return animation;
+}
+
+- (CAKeyframeAnimation *)secondHalfFlipAnimation:(CGRect)transAndScaleRect
+{
+    CATransform3D an0 = [self startOfSecondHalfTransform];
+    CATransform3D an1 = [self endOfSecondHalfTransform];
+    
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.values = @[[NSValue valueWithCATransform3D:an0],
+                         [NSValue valueWithCATransform3D:an1]];
+    animation.duration = 0.3;
+    animation.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut]];
+    
+    return animation;
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    if (anim == [self.imageView.layer animationForKey:@"FirstFlip"])
+    {
+        self.imageView2.layer.transform = [self startOfSecondHalfTransform];
+        self.imageView2.hidden = NO;
+        self.imageView.hidden = YES;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.imageView.layer removeAnimationForKey:@"FirstFlip"];
+            CAKeyframeAnimation *halfFlip2 = [self secondHalfFlipAnimation:[self createTransAndScaleRect]];
+            halfFlip2.removedOnCompletion = NO;
+            halfFlip2.delegate = self;
+            [self.imageView2.layer addAnimation:halfFlip2 forKey:@"SecondFlip"];
+        });
+        
+        return;
+    }
+    
+    if (anim == [self.imageView2.layer animationForKey:@"SecondFlip"])
+    {
+        self.imageView2.layer.transform = [self endOfSecondHalfTransform];
+        [self.imageView.layer removeAnimationForKey:@"SecondFlip"];
+        /*
+        CGFloat w = MIN(self.view.bounds.size.width, self.view.bounds.size.height);
+        self.imageView2.transform = CGAffineTransformScale(
+                                        CGAffineTransformMakeTranslation(
+                                            CGRectGetMidX(self.view.bounds) - CGRectGetMidX(self.miniFrame),
+                                            CGRectGetMidY(self.view.bounds) - CGRectGetMidY(self.miniFrame)),
+                                        w/self.miniFrame.size.width,
+                                        w/self.miniFrame.size.width);
+        */
+        return;
+    }
+}
+
+- (CATransform3D)endOfFirstHalfTransform
+{
+    CGRect transAndScaleRect = [self createTransAndScaleRect];
+    return CATransform3DRotate(
+                CATransform3DRotate(
+                    CATransform3DScale(
+                        CATransform3DMakeTranslation(transAndScaleRect.origin.x,
+                                                     transAndScaleRect.origin.y, 1000),
+                        transAndScaleRect.size.width,
+                        transAndScaleRect.size.height, 1.0),
+                    -90*M_PI/180.0, 0.0, 1.0, 0.0),
+                -90*M_PI/180.0, 0.0, 0.0, 1.0);
+}
+
+- (CATransform3D)startOfSecondHalfTransform
+{
+    CGRect transAndScaleRect = [self createTransAndScaleRect];
+    return CATransform3DRotate(
+                CATransform3DRotate(
+                    CATransform3DScale(
+                        CATransform3DMakeTranslation(transAndScaleRect.origin.x,
+                                                     transAndScaleRect.origin.y, 1000),
+                        transAndScaleRect.size.width,
+                        transAndScaleRect.size.height, 1.0),
+                    90*M_PI/180.0, 0.0, 1.0, 0.0),
+                90*M_PI/180.0, 0.0, 0.0, 1.0);
+}
+
+- (CATransform3D)endOfSecondHalfTransform
+{
+    CGRect transAndScaleRect = [self createTransAndScaleRect];
+    return CATransform3DScale(
+                CATransform3DMakeTranslation(transAndScaleRect.origin.x,
+                                             transAndScaleRect.origin.y, 1000),
+                transAndScaleRect.size.width,
+                transAndScaleRect.size.height, 1.0);
+}
+
+- (CGRect)createTransAndScaleRect
+{
+    CGFloat w = MIN(self.view.bounds.size.width, self.view.bounds.size.height);
+    return CGRectMake(CGRectGetMidX(self.view.bounds) - CGRectGetMidX(self.miniFrame),
+                      CGRectGetMidY(self.view.bounds) - CGRectGetMidY(self.miniFrame),
+                      w/self.miniFrame.size.width,
+                      w/self.miniFrame.size.width);
+}
+
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
+    CATransform3D perspective = CATransform3DIdentity;
+    perspective.m34 = 1.0 / - 180000.0;
+    self.view.layer.sublayerTransform = perspective;
+    
+    CAKeyframeAnimation *halfFlip = [self firstHalfFlipAnimation:[self createTransAndScaleRect]];
+    halfFlip.removedOnCompletion = NO;
+    halfFlip.delegate = self;
+    [self.imageView.layer addAnimation:halfFlip forKey:@"FirstFlip"];
+    //self.imageView.transform = CGAffineTransformIdentity;
+    
+    //self.imageView.hidden = NO;
+    //self.imageView2.hidden = NO;
     
     /*
     [UIView transitionFromView:self.imageView
@@ -103,13 +226,21 @@
                     }];
     */
     
-    [UIView animateWithDuration:0.4
+    [UIView animateWithDuration:0.6
                           delay:0.0
                         options:(UIViewAnimationOptionCurveEaseInOut)
                      animations:^{
                          self.view.backgroundColor = [UIColor blackColor];
-                     } completion:nil];
-    
+                     } completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.4
+                                               delay:0.0
+                                             options:(UIViewAnimationOptionCurveEaseInOut)
+                                          animations:^{
+                                              self.titleLabel.alpha = 1.0;
+                                              self.authorLabel.alpha = 1.0;
+                                          } completion:nil];
+                     }];
+    /*
     CGFloat w = MIN(self.view.bounds.size.width, self.view.bounds.size.height);
     [UIView animateWithDuration:0.3
                           delay:0.0
@@ -134,7 +265,7 @@
                                               self.titleLabel.alpha = 1.0;
                                               self.authorLabel.alpha = 1.0;
                                           } completion:nil];
-                     }];
+                     }];*/
 }
 
 - (void)viewWillAppear:(BOOL)animated
